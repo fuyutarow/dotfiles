ac2accf1fca96166f3c7b4e0b45ecb9f3139bf6a
fuyutarow <fuyutarow@gmail.com>
2018-10-10 07:16:15 +0900

    Add .comment_examples

--- sh/lns.sh
+++ sh/lns.sh
@@ -1,8 +1,11 @@
-ln -s dotfiles/.gitconfig ~
 ln -s dotfiles/.bashrc ~
 ln -s dotfiles/.bashrc.d ~
 ln -s dotfiles/.bash_aliases ~
 ln -s dotfiles/.vimrc ~
 ln -s dotfiles/.zshrc ~
 ln -s dotfiles/.tmuxrc ~
+
+ln -s dotfiles/.gitconfig ~
+ln -s dotfiles/.comment_exmaples.md ~
+
 ln -s dotfiles/.jsbeautifyrc ~ 

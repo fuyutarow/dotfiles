c0b9a64831893a45af88fa787b0677e9eb6038a7
fuyutarow <fuyutarow@gmail.com>
2018-10-13 22:22:43 +0900

    Switch zstyle in host

--- .zshrc.d/style.zsh
+++ .zshrc.d/style.zsh
@@ -1,5 +1,11 @@
-#zplug denysdovhan/spaceship-prompt, use:spaceship.zsh, from:github, as:theme
 #zplug sindresorhus/pure, use:pure.zsh, from:github, as:theme
-zplug "modules/osx", from:prezto, if:"[[ $OSTYPE == *darwin* ]]"
-zplug "modules/prompt", from:prezto
-zstyle ':prezto:module:prompt' theme 'giddie'
+case "$HOST" in
+  "KUROVO")
+    zplug "modules/osx", from:prezto, if:"[[ $OSTYPE == *darwin* ]]"
+    zplug "modules/prompt", from:prezto
+    zstyle ':prezto:module:prompt' theme 'giddie'
+;;
+  "conohatan")
+    zplug denysdovhan/spaceship-prompt, use:spaceship.zsh, from:github, as:theme
+;;
+esac

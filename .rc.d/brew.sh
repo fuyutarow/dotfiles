cee035acf62b86bc0640e28e5d93f2a72c1ada25
fuyutarow <fuyutarow@gmail.com>
2019-04-18 01:18:13 +0900

    Fix

--- .rc.d/brew.sh
+++ .rc.d/brew.sh
@@ -4,7 +4,7 @@ if [ -f ~/.env ]; then
   source <(sed -E -n 's/[^#]+/export &/ p' ~/.env)
   SHELL=$shell
   eval $($BREW_PATH/bin/brew shellenv)
-elif type "コマンド" > /dev/null 2>&1; then
+elif type brew > /dev/null 2>&1; then
   eval $(brew shellenv)
 fi
 

cf45d449caea3105d249d185fe6ab19d115b23bd
fuyutarow <fuyutarow@gmail.com>
2019-07-27 01:39:26 +0900

    Add setup.d/jupyter

---
+++ setup.d/jupyter.sh
@@ -0,0 +1,13 @@
+: ### install jupyter
+pip install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
+
+: ### vim bindings
+# Create required directory in case (optional)
+mkdir -p $(jupyter --data-dir)/nbextensions
+# Clone the repository
+pushd $(jupyter --data-dir)/nbextensions
+git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+# Activate the extension
+jupyter nbextension enable vim_binding/vim_binding
+popd

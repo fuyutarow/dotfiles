05049936aef914150ecb79d07cfad7a743d1405f
fuyutarow <fuyutarow@gmail.com>
2019-08-28 02:06:01 +0900

    Update prompt

--- .zshrc.d/style.zsh
+++ .zshrc.d/style.zsh
@@ -12,11 +12,18 @@ case "$HOST" in
     zplug denysdovhan/spaceship-prompt, use:spaceship.zsh, from:github, as:theme
   ;;
   "ringbell"|"ringbell.local")
+    uname="%F{magenta}%n%f"
+    host="%F{yellow}%m%f"
+    pwd="%F{green}%~%f"
+    NEWLINE=$'\n'
+    prompt="%F{blue})%f "
+
+    PROMPT="${uname}@${host}|${pwd}${NEWLINE}${prompt}"
+    # RPROMPT="%F{cyan}%D{%m-%d %H:%M}%f"
+  ;;
+ *)
     zplug "modules/osx", from:prezto, if:"[[ $OSTYPE == *darwin* ]]"
     zplug "modules/prompt", from:prezto
     zstyle ':prezto:module:prompt' theme 'giddie'
-  ;;
-  *)
-    zplug denysdovhan/spaceship-prompt, use:spaceship.zsh, from:github, as:theme
   ;;
 esac

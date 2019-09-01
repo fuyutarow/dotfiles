923b33cd3c92e5c9f2d28387259c5d9bbf070194
fuyutarow <fuyutarow@gmail.com>
2019-09-01 13:56:19 +0900

    Update env.d

---
+++ .env.d/tex.sh
@@ -0,0 +1,1 @@
+export PATH=$HOMEBREW_PREFIX/Cellar/texlive/20181114/libexec/bin/x86_64-linux:$PATH
---
+++ .env.d/wasmer.sh
@@ -0,0 +1,4 @@
+# Wasmer
+export WASMER_DIR="/Users/fuyutarow/.wasmer"
+[ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"  # This loads wasmer
+

961b407aeef46a66d8120c89891f09a7f247d166
fuyutarow <fuyutarow@gmail.com>
2019-12-04 02:32:25 +0900

    Fix rcfile

--- setup.d/install_user.sh
+++ setup.d/install_user.sh
@@ -4,8 +4,8 @@ function rcfile () {
  case "${SHELL##*/}" in
    "bash" ) echo bashrc ;;
    "zsh" ) echo zshrc ;;
-   "cshrc" ) echo cshrc ;;
-   "tcshrc" ) echo tcshrc ;;
+   "csh" ) echo cshrc ;;
+   "tcsh" ) echo tcshrc ;;
    "*" ) echo
  esac
 }
@@ -18,7 +18,7 @@ make link
 git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
 mkdir ~/.linuxbrew/bin
 ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
-echo "$(~/.linuxbrew/bin/brew shellenv)" >> "~/.$(rcfile)"
+echo "$(~/.linuxbrew/bin/brew shellenv)" >> ~/.$(rcfile)
 
 SHELL=bash
 eval $(~/.linuxbrew/bin/brew shellenv)

757f10387ebf9b64de03d5ade62d64502c961406
fuyutarow <fuyutarow@gmail.com>
2020-01-27 12:53:04 +0900

    Up for matechax

--- init-linux.sh
+++ init-linux.sh
@@ -1,6 +1,7 @@
 sudo apt update 
-sudo apt upgrade 
+sudo apt upgrade -y
 sudo apt install -y build-essential curl file git
+sudo apt install -y libssl-dev
 make link
 make add-rust
 source $HOME/.cargo/env
